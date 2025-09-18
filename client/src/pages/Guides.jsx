42dd163ea082c3497abfd69cfbea2e263c2e102f
avinasdube <avidubey712@gmail.com>
2025-09-18 13:54:21 +0530

    feat: create Guides component for displaying curated travel guides

---
+++ client/src/pages/Guides.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+
+const Guides = () => (
+  <>
+    <Navbar />
+    <div style={{ padding: "2rem" }}>
+      <h1>Travel Guides</h1>
+      <p>Explore curated travel guides here.</p>
+    </div>
+  </>
+);
+
+export default Guides;
