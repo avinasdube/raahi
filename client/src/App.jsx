bc6545292e2f6839e8f2f3f25dc84baa20f81cf5
avinasdube <avidubey712@gmail.com>
2025-09-18 13:41:13 +0530

    Refactor App component: remove unused code and implement routing with React Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Auth from "./pages/Auth";
+import Budget from "./pages/Budget";
+import Home from "./pages/Home";
+import HotelDetails from "./pages/HotelDetails";
+import Hotels from "./pages/Hotels";
+import Management from "./pages/Management";
+import Marketplace from "./pages/Marketplace";
+import Planner from "./pages/Planner";
+import Safety from "./pages/Safety";
+import Saved from "./pages/Saved";
+import SearchResults from "./pages/SearchResults";
+import Trips from "./pages/Trips";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<SearchResults />} />
+        <Route path="/hotel/:id" element={<HotelDetails />} />
+        <Route path="/hotels" element={<Hotels />} />
+        <Route path="/trips" element={<Trips />} />
+        <Route path="/saved" element={<Saved />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/planner" element={<Planner />} />
+        <Route path="/marketplace" element={<Marketplace />} />
+        <Route path="/management" element={<Management />} />
+        <Route path="/safety" element={<Safety />} />
+        <Route path="/budget" element={<Budget />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
