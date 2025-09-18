f48eb6895c9cfd317eb9a8aae8fcc475507e5012
avinasdube <avidubey712@gmail.com>
2025-09-18 23:36:35 +0530

    feat: reorganize route imports for better clarity and structure

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
+
 import authRouter from "./auth.routes.js";
+import dataRouter from "./data.routes.js";
 
 const router = Router();
 
 router.use("/auth", authRouter);
+router.use("/", dataRouter);
 
 export default router;
